1f775ce82dab510c0ea77a72948f001f1cbe306c
Stephen Hara <sthara216@gmail.com>
2017-10-05 22:45:43 -0400

    add seed for test data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule BandTogetherApp.DatabaseSeeder do
+    alias BandTogetherApp.Repo
+
+    alias BandTogetherApp.Musicians.User, as: BUser
+
+    @dev_users [ %BUser{first_name: "Joe", last_name: "Hogs", email: "j@hoggo.com", password: "nil", biography: "j hogs"}, \
+        %BUser{first_name: "Theodore", last_name: "ZORK", email: "zork@zorric.com", password: "nil", biography: "ZORK"}, \
+        %BUser{first_name: "Wilma", last_name: "Jinkies", email: "isaidit@jinkies.com", password: "nil", biography: "it must be a clue"} \
+    ]
+
+    def seed_dev() do
+        Enum.each(@dev_users, fn (person) -> Repo.insert!(person) end)
+    end
+end
+
+BandTogetherApp.DatabaseSeeder.seed_dev
