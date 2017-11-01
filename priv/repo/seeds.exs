2537f70d945305c223fa209b2dcfe25b3a3f7833
Stephen Hara <sthara216@gmail.com>
2017-11-01 17:38:42 -0400

    update db seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -8,7 +8,6 @@
 #     BandTogetherApp.Repo.insert!(%BandTogetherApp.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
 
 defmodule BandTogetherApp.DatabaseSeeder do
     alias BandTogetherApp.Repo
@@ -18,36 +17,96 @@ defmodule BandTogetherApp.DatabaseSeeder do
     alias BandTogetherApp.Musicians.Portfolio, as: BPortfolio
     alias BandTogetherApp.Musicians.Band, as: BBand
 
-    @dev_users [
-        %BUser{first_name: "Joe", last_name: "Hogs", email: "j@hoggo.com", password: "nil", biography: "j hogs", stage_name: "Hogger"}, \
-        %BUser{first_name: "Theodore", last_name: "ZORK", email: "zork@zorric.com", password: "nil", biography: "ZORK", stage_name: "Party Cannon"}, \
-        %BUser{first_name: "Wilma", last_name: "Jinkies", email: "isaidit@jinkies.com", password: "nil", biography: "it must be a clue", stage_name: "Mystery Machine"} \
-    ]
+    @guitar_talent %BTalent{title: "Guitar", img_filepath: "guitar.png"}
+    @drum_talent %BTalent{title: "Drums", img_filepath: "drums.png"}
+    @mixing_talent %BTalent{title: "Mixing", img_filepath: "mixer.png"}
+    @vocals_talent %BTalent{title: "Vocals", img_filepath: "vocals.png"}
+    @composing_talent %BTalent{title: "Composign", img_filepath: "composing.png"}
 
-    @dev_talents [
-        %BTalent{title: "Guitar", img_filepath: "guitar.png"}, \
-        %BTalent{title: "Drums", img_filepath: "drums.png"}, \
-        %BTalent{title: "Mixing", img_filepath: "mixer.png"}
-    ]
+    @dev_talents [@guitar_talent, @drum_talent, @mixing_talent, @vocals_talent, @composing_talent]
 
-    @dev_portfolios [
-        %BPortfolio{title: "Isucon Revenge", url: "http://isu.con"}, \
-        %BPortfolio{title: "Woodstock", url: "http://wood.stock"}, \
-        %BPortfolio{title: "Rime of the Ancient Mariner", url: "http://mad.rimez"}, \
-        %BPortfolio{title: "Brave New World", url: "http://ironmaid.en"}, \
-        %BPortfolio{title: "OC Remix", url: "https://soundcloud.com/ocremix/sets/mirror-image-a-link-to-the-past-remixed"}
-    ]
+    @joe_hogs %BUser{
+        first_name: "Joe",
+        last_name: "Hogs",
+        email: "j@hoggo.com",
+        password: "nil",
+        biography: "j hogs",
+        stage_name: "Hogger",
+        talents: [@guitar_talent, @drum_talent]
+    }
+    @t_zork %BUser{
+        first_name: "Theodore",
+        last_name: "ZORK",
+        email: "zork@zorric.com",
+        password: "nil",
+        biography: "ZORK",
+        stage_name: "Party Cannon",
+        talents: [@guitar_talent, @mixing_talent]
+    }
+    @wilma %BUser{
+        first_name: "Wilma",
+        last_name: "Jinkies",
+        email: "isaidit@jinkies.com",
+        password: "nil",
+        biography: "it must be a clue",
+        stage_name: "Mystery Machine",
+        talents: [@mixing_talent]
+    }
+    @charles_barkley %BUser{
+        first_name: "Charles",
+        last_name: "Barkley",
+        email: "slam@jam.com",
+        password: "nil",
+        biography: "DJ? TURN IT UP",
+        stage_name: "Space Jam",
+        talents: [@vocals_talent, @composing_talent]
+    }
+    @brucey_d %BUser{
+        first_name: "Bruce",
+        last_name: "Dickinson",
+        email: "bruced@thetrooper.com",
+        password: "nil",
+        biography: "YOU'LL TAKE MY LIFE BUT I'LL TAKE YOURS TOO",
+        stage_name: "The Trooper",
+        talents: [@vocals_talent, @composing_talent]
+    }
+    @dev_users [@joe_hogs, @t_zork, @wilma, @brucey_d, @charles_barkley]
 
-    @dev_bands [
-        %BBand{biography: "hello", description: "We are cool", stage_name: "The Cool Kids", loc_country: "USA"}, \
-        %BBand{biography: "mewtwo", description: "We are cooler", stage_name: "The Cooler Kids", loc_country: "Spain"}, \
-        %BBand{biography: "hobbit", description: "We are coolest", stage_name: "The Coolest Kids", loc_country: "China"}
-    ]
+    @isucon %BPortfolio{title: "Isucon Revenge", url: "http://isu.con"}
+    @woodstock %BPortfolio{title: "Woodstock", url: "http://wood.stock"}
+    @rotma %BPortfolio{title: "Rime of the Ancient Mariner", url: "http://mad.rimez"}
+    @brave_new_world %BPortfolio{title: "Brave New World", url: "http://ironmaid.en"}
+    @oc_remix %BPortfolio{title: "OC Remix", url: "https://soundcloud.com/ocremix/sets/mirror-image-a-link-to-the-past-remixed"}
+
+    @dev_portfolios [@isucon, @woodstock, @rotma, @brave_new_world, @oc_remix]
+
+    @iron_maiden %BBand{
+        biography: "FUCKING METAL",
+        description: "WE ARE IRON MAIDEN",
+        stage_name: "Iron Maiden",
+        loc_country: "United Kingdom",
+        users: [@brucey_d]
+    }
+    @megadeth %BBand{
+        biography: "mewtwo",
+        description: "We are cooler",
+        stage_name: "The Cooler Kids",
+        loc_country: "Spain",
+        users: [@joe_hogs, @t_zork, @wilma]
+    }
+
+    @quad_city_djs %BBand{
+        biography: "GO GO POWER RANGERS",
+        description: "Y'all ready for this?",
+        stage_name: "Quad City DJs",
+        loc_country: "United States of Jam",
+        users: [@charles_barkley]
+    }
+
+    @dev_bands [@iron_maiden, @megadeth, @quad_city_djs]
 
 
     def seed_dev() do
-        Enum.each(@dev_users, fn (person) -> Repo.insert!(person) end)
-        Enum.each(@dev_talents, fn (talent) -> Repo.insert!(talent) end)
         Enum.each(@dev_portfolios, fn (talent) -> Repo.insert!(talent) end)
         Enum.each(@dev_bands, fn (talent) -> Repo.insert!(talent) end)
     end
