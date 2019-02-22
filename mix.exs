d58fe2dc0598ff37aa0a40dec6a27792daedeb4c
Stephen Hara <sthara216@gmail.com>
2019-02-22 14:09:49 +0900

    CSS recompiles and hot reloads now

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule BandTogetherApp.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3.2"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
@@ -41,6 +41,7 @@ defmodule BandTogetherApp.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:cors_plug, "~> 1.2"},
       {:secure_random, "~> 0.5.1"},
       {:comeonin, "~> 4.0.3"},
