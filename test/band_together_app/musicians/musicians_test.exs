91dc670fa4071b88c14f24ffbf222da8f7607b31
Stephen Hara <sthara216@gmail.com>
2019-03-19 22:59:26 +0900

    add the backend bits for tags (#7)

--- test/band_together_app/musicians/musicians_test.exs
+++ test/band_together_app/musicians/musicians_test.exs
@@ -266,4 +266,62 @@ defmodule BandTogetherApp.MusiciansTest do
       assert %Ecto.Changeset{} = Musicians.change_band(band)
     end
   end
+
+  describe "tags" do
+    alias BandTogetherApp.Musicians.Tag
+
+    @valid_attrs %{}
+    @update_attrs %{}
+    @invalid_attrs %{}
+
+    def tag_fixture(attrs \\ %{}) do
+      {:ok, tag} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Musicians.create_tag()
+
+      tag
+    end
+
+    test "list_tags/0 returns all tags" do
+      tag = tag_fixture()
+      assert Musicians.list_tags() == [tag]
+    end
+
+    test "get_tag!/1 returns the tag with given id" do
+      tag = tag_fixture()
+      assert Musicians.get_tag!(tag.id) == tag
+    end
+
+    test "create_tag/1 with valid data creates a tag" do
+      assert {:ok, %Tag{} = tag} = Musicians.create_tag(@valid_attrs)
+    end
+
+    test "create_tag/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Musicians.create_tag(@invalid_attrs)
+    end
+
+    test "update_tag/2 with valid data updates the tag" do
+      tag = tag_fixture()
+      assert {:ok, tag} = Musicians.update_tag(tag, @update_attrs)
+      assert %Tag{} = tag
+    end
+
+    test "update_tag/2 with invalid data returns error changeset" do
+      tag = tag_fixture()
+      assert {:error, %Ecto.Changeset{}} = Musicians.update_tag(tag, @invalid_attrs)
+      assert tag == Musicians.get_tag!(tag.id)
+    end
+
+    test "delete_tag/1 deletes the tag" do
+      tag = tag_fixture()
+      assert {:ok, %Tag{}} = Musicians.delete_tag(tag)
+      assert_raise Ecto.NoResultsError, fn -> Musicians.get_tag!(tag.id) end
+    end
+
+    test "change_tag/1 returns a tag changeset" do
+      tag = tag_fixture()
+      assert %Ecto.Changeset{} = Musicians.change_tag(tag)
+    end
+  end
 end
