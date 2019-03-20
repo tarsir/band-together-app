a52606a5cfa83dfb19ff00aa1dac02b365ace12e
Stephen Hara <stephen.hara@stephenhara.com>
2019-03-20 22:03:51 +0900

    update configs for distillery

--- config/prod.exs
+++ config/prod.exs
@@ -2,15 +2,18 @@ use Mix.Config
 
 config :band_together_app, BandTogetherAppWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "band-together-app.herokuapp.com", port: 443],
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "${APP_NAME}.gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: "${SECRET_KEY_BASE}"
 
 config :band_together_app, BandTogetherApp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  url: "${DATABASE_URL}",
+  database: "",
+  pool_size: 2,
   ssl: true
 
 # Do not print debug messages in production
