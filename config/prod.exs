edf096831ac58a4012a1ebecd5572bf3df9c80b6
Stephen Hara <stephen.hara@stephenhara.com>
2019-03-20 23:03:33 +0900

    more config

--- config/prod.exs
+++ config/prod.exs
@@ -2,19 +2,19 @@ use Mix.Config
 
 config :band_together_app, BandTogetherAppWeb.Endpoint,
   load_from_system_env: true,
-  server: true,
   http: [port: {:system, "PORT"}],
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
   url: [scheme: "https", host: "${APP_NAME}.gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: "${SECRET_KEY_BASE}"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :band_together_app, BandTogetherApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: "${DATABASE_URL}",
   database: "",
-  pool_size: 2,
-  ssl: true
+  ssl: true,
+  pool_size: 2
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule BandTogetherApp.Mixfile do
   def application do
     [
       mod: {BandTogetherApp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
